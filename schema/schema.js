1633facae1370ca5772b507995cdda0ff4e3512b
Morkowski <wojciech.morkowski@gmail.com>
2018-03-28 20:48:52 +0200

    init

---
+++ schema/schema.js
@@ -0,0 +1,44 @@
+const graphQl = require('graphql');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphQl;
+
+const find = require('lodash/find');
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20 },
+    { id: '47', firstName: 'Samantha', age: 21 },
+]
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type:GraphQLString },
+        age: { type: GraphQLInt }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, args) {
+                return find(users, { id: args.id })
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
